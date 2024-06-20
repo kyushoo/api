8127fcf6a84e3ef3d96e3bfd4ede0ebbd4e1281f
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-06-21 01:42:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
